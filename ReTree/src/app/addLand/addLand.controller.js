b06a453423666a4ef482f238a1639195b1516308
Daniel Berrocal <jdanielb1992@gmail.com>
2016-07-01 15:09:01 -0600

    places

--- ReTree/src/app/addLand/addLand.controller.js
+++ ReTree/src/app/addLand/addLand.controller.js
@@ -3,12 +3,12 @@
   var controllerId = 'addLandController';
   angular
     .module('app')
-        .controller(controllerId, ['$scope','$state',AddLand]);
+        .controller(controllerId, ['$scope','$state','ErrorService',AddLand]);
 
-    function AddLand ($scope,$state)
+    function AddLand ($scope,$state,ErrorService)
     {
       var vm = this;
-
+      vm.errorTemplate = ErrorService.errorTemplate;
       vm.cancel = function cancel() {
           $state.go('tab.lands');
       };
