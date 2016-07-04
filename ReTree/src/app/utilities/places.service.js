5ae3e3126ea70b5d0ab3a78cb7c0bd506ab546f0
Daniel Berrocal <jdanielb1992@gmail.com>
2016-07-04 14:39:12 -0600

    places

--- ReTree/src/app/utilities/places.service.js
+++ ReTree/src/app/utilities/places.service.js
@@ -97,31 +97,200 @@
               id: 2,
               name:'Alajuela',
               cantons:[
-
+                  {
+                      name:'Alajuela',
+                      districts:['Alajuela','San José','Carrizal','San Antonio','Guácima','San Isidro','Sabanilla','San Rafael','Río Segundo','Desamparados','Turrúcares','Tambor','Garita','Sarapiquí']
+                  },
+                  {
+                      name:'San Ramón',
+                      districts:['San Ramón','Santiago','San Juan','Piedades Norte','Piedades Sur','San Rafael','San Isidro','Ángeles ','Alfaro','Volio','Concepción','Zapotal','Peñas Blancas']
+                  },
+                  {
+                      name:'Grecia',
+                      districts:['Grecia','San Isidro','San José','San Roque','Tacares','Río Cuarto','Puente de Piedra','Bolívar']
+                  },
+                  {
+                      name:'San Mateo',
+                      districts:['San Mateo','Desmonte','Jesús María','Labrador']
+                  },
+                  {
+                      name:'Atenas',
+                      districts:['Atenas','Jesús','Mercedes','San Isidro','Concepción','San José','Santa Eulalia','Escobal']
+                  },
+                  {
+                      name:'Naranjo',
+                      districts:['Naranjo','San Miguel','San José','Cirrí Sur','San Jerónimo','San Juan','El Rosario','Palmitos']
+                  },
+                  {
+                      name:'Palmares',
+                      districts:['Palmares','Zaragoza','Buenos Aires','Santiago','Candelaria','Esquipulas','La Granja']
+                  },
+                  {
+                      name:'Poás',
+                      districts:['San Pedro','San Juan','San Rafael','Carrillos','Sabana Redonda']
+                  },
+                  {
+                      name:'Orotina',
+                      districts:['Orotina','El Mastate','Hacienda Vieja','Coyolar','La Ceiba']
+                  },
+                  {
+                      name:'San Carlos',
+                      districts:['Quesada','Florencia','Buenavista','Aguas Zarcas','Venecia','Pital','La Fortuna','La Tigra','La Palmera','Venado','Cutris','Monterrey','Pocosol']
+                  },
+                  {
+                      name:'Zarcero',
+                      districts:['Zarcero','Laguna','Tapesco','Guadalupe','Palmira','Zapote','Brisas']
+                  },
+                  {
+                      name:'Valverde Vega',
+                      districts:['Sarchí Norte','Sarchí Sur','Toro Amarillo','San Pedro','Rodríguez']
+                  },
+                  {
+                      name:'Upala',
+                      districts:['Upala','Aguas Claras','San José o Pizote','Bijagua','Delicias','Dos Ríos','Yolillal','Canalete']
+                  },
+                  {
+                      name:'Los Chiles',
+                      districts:['Los Chiles','Caño Negro','El Amparo','San Jorge']
+                  },
+                  {
+                      name:'Guatuso',
+                      districts:['San Rafael','Buenavista','Cote','Katira']
+                  }
               ]
-
           },
           {
               id: 3,
               name:'Cartago',
               cantons:[
-                  
+                  {
+                      name:'Cartago',
+                      districts:['Oriental','Occidental','Carmen','San Nicolás','Aguacaliente o San Francisco','Guadalupe o Arenilla','Corralillo','Tierra Blanca','Dulce Nombre','Llano Grande','Quebradilla']
+                  },
+                  {
+                      name:'Paraíso',
+                      districts:['Paraíso','Santiago','Orosi','Cachí','Llanos de Santa Lucía']
+                  },
+                  {
+                      name:'La Unión',
+                      districts:['Tres Ríos','San Diego','San Juan','San Rafael','Concepción','Dulce Nombre','San Ramón','Río Azul']
+                  },
+                  {
+                      name:'Jiménez',
+                      districts:['Juan Viñas','Tucurrique','Pejibaye']
+                  },
+                  {
+                      name:'Turrialba',
+                      districts:['Turrialba','La Suiza','Peralta','Santa Cruz','Santa Teresita','Pavones','Tuis','Tayutic','Santa Rosa','Tres Equis','La Isabel','Chirripó']
+                  },
+                  {
+                      name:'Alvarado',
+                      districts:['Pacayas','Cervantes','Capellades']
+                  },
+                  {
+                      name:'Oreamuno',
+                      districts:['San Rafael','Cot','Potrero Cerrado','Cipreses','Santa Rosa']
+                  },
+                  {
+                      name:'El Guarco',
+                      districts:['El Tejar','San Isidro','Tobosi','Patio de Agua']
+                  }
               ]
-
           },
           {
               id: 4,
               name:'Heredia',
               cantons:[
-                  
+                  {
+                      name:'Heredia',
+                      districts:['Heredia','Mercedes','San Francisco','Ulloa ','Varablanca']
+                  },
+                  {
+                      name:'Barva',
+                      districts:['Barva','San Pedro','San Pablo','San Roque','Santa Lucía','San José de la Montaña']
+                  },
+                  {
+                      name:'Santo Domingo',
+                      districts:['Santo Domingo','San Vicente','Paracito','Santo Tomás','Santa Rosa','Tures','Pará']
+                  },
+                  {
+                      name:'Santa Bárbara',
+                      districts:['Santa Bárbara','San Pedro','San Juan','Jesús','Santo Domingo','Purabá']
+                  },
+                  {
+                      name:'San Rafael',
+                      districts:['San Rafael','San Josecito','Santiago','Ángeles','Concepción']
+                  },
+                  {
+                      name:'San Isidro',
+                      districts:['San Isidro','San José','Concepción','San Francisco']
+                  },
+                  {
+                      name:'Belén',
+                      districts:['San Antonio','La Ribera','La Asunción']
+                  },
+                  {
+                      name:'Flores',
+                      districts:['San Joaquín','Barrantes ','Llorente']
+                  },
+                  {
+                      name:'San Pablo',
+                      districts:['San Pablo','Rincón de Sabanilla']
+                  },
+                  {
+                      name:'Sarapiquí',
+                      districts:['Puerto Viejo','La Virgen','Las Horquetas','Llanuras del Gaspar','Cureña']
+                  }
               ]
-
           },
           {
               id: 5,
               name:'Guanacaste',
               cantons:[
-                  
+                  {
+                      name:'Liberia',
+                      districts:['Liberia','Cañas Dulces','Mayorga','Nacascolo','Curubandé']
+                  },
+                  {
+                      name:'Nicoya',
+                      districts:['Nicoya','Mansión','San Antonio','Quebrada Honda','Sámara','Nosara','Belén de Nosarita']
+                  },
+                  {
+                      name:'Santa Cruz',
+                      districts:['Santa Cruz','Bolsón','Veintisiete de Abril','Tempate','Cartagena','Cuajiniquil','Diriá','Cabo Velas ','Tamarindo']
+                  },
+                  {
+                      name:'Bagaces',
+                      districts:['Bagaces','La Fortuna','Mogote','Río Naranjo']
+                  },
+                  {
+                      name:'Carrillo',
+                      districts:['Filadelfia','Palmira','Sardinal','Belén']
+                  },
+                  {
+                      name:'Cañas',
+                      districts:['Cañas','Palmira','San Miguel','Bebedero','Porozal']
+                  },
+                  {
+                      name:'Abangares',
+                      districts:['Las Juntas','Sierra','San Juan ','Colorado']
+                  },
+                  {
+                      name:'Tilarán',
+                      districts:['Tilarán','Quebrada Grande','Tronadora','Santa Rosa','Líbano','Tierras Morenas','Arenal']
+                  },
+                  {
+                      name:'Nandayure',
+                      districts:['Carmona','Santa Rita','Zapotal','San Pablo','San Pablo','Porvenir','Bejuco']
+                  },
+                  {
+                      name:'La Cruz',
+                      districts:['La Cruz','Santa Cecilia','La Garita','Santa Elena']
+                  },
+                  {
+                      name:'Hojancha',
+                      districts:['Hojancha','Monte Romo','Puerto Carrillo','Huacas']
+                  }
               ]
 
           },
@@ -129,7 +298,50 @@
               id: 6,
               name:'Puntarenas',
               cantons:[
-                  
+                  {
+                      name:'Puntarenas',
+                      districts:['Puntarenas','Pitahaya','Chomes','Lepanto','Paquera','Manzanillo','Guacimal','Barranca','Monte Verde','Isla del Coco','Cóbano','Chacarita','Chira','Acapulco','El Roble','Arancibia']
+                  },
+                  {
+                      name:'Esparza',
+                      districts:['Espíritu Santo ','San Juan Grande','Macacona','San Rafael','San Jerónimo']
+                  },
+                  {
+                      name:'Buenos Aires',
+                      districts:['Buenos Aires','Volcán','Potrero Grande','Boruca','Pilas','Colinas','Chánguena','Biolley','Brunka']
+                  },
+                  {
+                      name:'Montes de Oro',
+                      districts:['Miramar','La Unión','San Isidro']
+                  },
+                  {
+                      name:'Osa',
+                      districts:['Puerto Cortés','Palmar','Sierpe','Bahía Ballena','Piedras Blancas','Bahía Drake']
+                  },
+                  {
+                      name:'Quepos',
+                      districts:['Quepos','Savegre','Naranjito']
+                  },
+                  {
+                      name:'Golfito',
+                      districts:['Golfito','Puerto Jiménez','Guaycará','Pavón']
+                  },
+                  {
+                      name:'Coto Brus',
+                      districts:['San Vito','Sabalito','Agua Buena','Limoncito','Pittier']
+                  },
+                  {
+                      name:'Parrita',
+                      districts:['Parrita']
+                  },
+                  {
+                      name:'Corredores',
+                      districts:['Corredor','La Cuesta','Canoas','Laurel']
+                  },
+                  {
+                      name:'Garabito',
+                      districts:['Jacó','Tárcoles']
+                  }
               ]
 
           },
@@ -165,5 +377,7 @@
 
           }
       ];
+
+
     }
 })();
